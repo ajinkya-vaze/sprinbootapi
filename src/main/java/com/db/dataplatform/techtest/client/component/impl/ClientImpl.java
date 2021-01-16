6c7f7d52062fc444289dec23860f288c2d65789d
Ajinkya Vaze <ajinkyav@gmail.com>
2021-01-16 14:27:16 +0000

    Exception handling for push data http client.

--- src/main/java/com/db/dataplatform/techtest/client/component/impl/ClientImpl.java
+++ src/main/java/com/db/dataplatform/techtest/client/component/impl/ClientImpl.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplate;
 
@@ -30,13 +31,18 @@ public class ClientImpl implements Client {
     public boolean pushData(DataEnvelope dataEnvelope) {
         log.info("Pushing data {} to {}", dataEnvelope.getDataHeader().getName(), URI_PUSHDATA);
         HttpEntity<DataEnvelope> request = new HttpEntity<>(dataEnvelope);
-        boolean response = restTemplate.postForObject(URI_PUSHDATA, request, Boolean.class);
-        log.info("Successfully pushed data {} to server. Checksum check {}.", dataEnvelope.getDataHeader().getName(), getChecksumStatus(response));
-        return response;
+        try {
+            boolean response = restTemplate.postForObject(URI_PUSHDATA, request, Boolean.class);
+            log.info("Successfully pushed data {} to server. Checksum check {}.", dataEnvelope.getDataHeader().getName(), getChecksumStatus(response));
+            return response;
+        } catch (RestClientException rce) {
+            log.error("Exception while pushing data {} to server", dataEnvelope.getDataHeader().getName(), rce);
+            return false;
+        }
     }
 
     private String getChecksumStatus(boolean passed) {
-        return passed ? "Passed": "Failed";
+        return passed ? "Passed" : "Failed";
     }
 
     @Override
--- src/test/java/com/db/dataplatform/techtest/client/component/impl/ClientImplTest.java
+++ src/test/java/com/db/dataplatform/techtest/client/component/impl/ClientImplTest.java
@@ -4,35 +4,62 @@ import com.db.dataplatform.techtest.client.api.model.DataEnvelope;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ClientImplTest {
 
     @Mock
-    private RestTemplate restTemplateMock;
+    private RestTemplate restTemplate;
 
-    @InjectMocks
     private ClientImpl client;
 
     private DataEnvelope testDataEnvelope;
 
-
     @Before
     public void setUp() {
+        client = new ClientImpl(restTemplate);
         testDataEnvelope = TestClientDataHelper.createTestDataEnvelopeApiObject();
     }
 
     @Test
     public void pushDataShouldWorkWhenHttpCallIsSuccessful() {
-        when(restTemplateMock.postForObject(ClientImpl.URI_PUSHDATA, testDataEnvelope, Boolean.class)).thenReturn(true);
+        when(restTemplate.postForObject(any(String.class), any(HttpEntity.class), ArgumentMatchers.<Class<Boolean>>any())).thenReturn(true);
         boolean response = client.pushData(testDataEnvelope);
         assertTrue(response);
+    }
+
+    @Test
+    public void pushDataReturnsFalseWhenHttpCallResultsInClientException() {
+        when(restTemplate.postForObject(any(String.class), any(HttpEntity.class), ArgumentMatchers.<Class<Boolean>>any())).thenThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST));
+        boolean response = client.pushData(testDataEnvelope);
+        assertFalse(response);
+    }
+
+    @Test
+    public void pushDataReturnsFalseWhenHttpCallResultsInServerException() {
+        when(restTemplate.postForObject(any(String.class), any(HttpEntity.class), ArgumentMatchers.<Class<Boolean>>any())).thenThrow(new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR));
+        boolean response = client.pushData(testDataEnvelope);
+        assertFalse(response);
+    }
+
+    @Test
+    public void pushDataReturnsFalseWhenRestClientFails() {
+        when(restTemplate.postForObject(any(String.class), any(HttpEntity.class), ArgumentMatchers.<Class<Boolean>>any())).thenThrow(new RestClientException("Exception while creating the request"));
+        boolean response = client.pushData(testDataEnvelope);
+        assertFalse(response);
     }
 }
